0f7a01df77e6578040ad38bcc87cc99d3fe86c3b
Andrey Obibok <andreyobibok@gmail.com>
2022-12-25 20:16:40 +0200

    hw fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,6 +11,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+let timerID;
+
 function handleStart() {
     timerID = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
