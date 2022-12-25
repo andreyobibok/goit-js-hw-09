54e9b1991e3e8f9cf9ac4ea3bea8ce1b2258acc3
Andrey Obibok <andreyobibok@gmail.com>
2022-12-25 10:28:35 +0200

    task 1 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+startButton.addEventListener('click', handleStart);
+stopButton.addEventListener('click', handleStop);
+
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function handleStart() {
+    timerID = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function handleStop() {
+    clearInterval(timerID);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
