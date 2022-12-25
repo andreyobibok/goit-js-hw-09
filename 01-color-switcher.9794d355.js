70cef209420078a93573cf18b63ba03cd33796ec
andreyobibok <108631116+andreyobibok@users.noreply.github.com>
2022-12-25 18:17:10 +0000

    Deploying to gh-pages from @ andreyobibok/goit-js-hw-09@0f7a01df77e6578040ad38bcc87cc99d3fe86c3b 🚀

--- 01-color-switcher.9794d355.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){timerID=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(timerID),e.disabled=!1,t.disabled=!0})),t.disabled=!0;
-//# sourceMappingURL=01-color-switcher.9794d355.js.map
---
+++ 01-color-switcher.b67cedf4.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){e=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(e),t.disabled=!1,d.disabled=!0})),d.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.b67cedf4.js.map
